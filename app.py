a13b3894d3789d2e85ba642db4dba147d05dfd63
Pedro Luiz Cantarelli Junior <pcantarelli@hotmail.com>
2021-05-07 19:05:50 +0100

    Add exercise button on Program page

--- app.py
+++ app.py
@@ -280,5 +280,5 @@ def page_server_error(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT")),
-    debug=True) # trocar para False
+    debug=False)
 
